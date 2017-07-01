a23fb42a161ba1af0842882823c8dd2c351aa95f
Walderlan Sena <senawalderlan@gmail.com>
2017-07-01 02:53:39 -0300

    Modificado a identação do código e adiciona comentarios para a orientação

--- helloword/helloword.asm
+++ helloword/helloword.asm
@@ -6,21 +6,21 @@
 
 section .data
 
-msg: db "Hello Word !", 0x0a ; Caracter de nova linha
-tam: equ $-msg               ; Posição atual menos o endereço de msg
+  msg: db "Hello Word !", 0x0a ; Caracter de nova linha
+  tam: equ $-msg               ; Posição atual menos o endereço de msg
 
 section .text
-global _start
+  global _start
 
 _start:
 
-mov edx, tam  ; count
-mov ecx, msg  ; *buf
-mov ebx, 1    ; fd
-mov eax, 4    ; numero da syscall write()
-int 0x80
+  mov edx, tam  ; count
+  mov ecx, msg  ; *buf
+  mov ebx, 1    ; fd
+  mov eax, 4    ; numero da syscall write()
+  int 0x80      ; chamando o kernel
 
-; void _exit(int status);
-mov ebx, 0    ; valor do retorno da exit()
-mov eax, 1    ; codigo da syscall exit()
-int 0x80
+  ; void _exit(int status);
+  mov ebx, 0    ; valor do retorno da exit()
+  mov eax, 1    ; codigo da syscall exit()
+  int 0x80      ; chamando o kernel
